2b8abac836832c8834f643dbc380815eb8c2287c
Jonathan <131607773+ClearlyJonathan@users.noreply.github.com>
2025-11-05 11:32:47 +0100

    Implemented search of builtin.
    Fixed the error where search isn't properly configured.
    Builtins fixed
    Status check implemented

--- task.c
+++ task.c
@@ -3,26 +3,28 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <stdbool.h>
 #include <sys/wait.h>
-
+#include <errno.h>
 // Methods
 void interface();
-int builtIn();
+int startsWith(const char* str, const char* prefix);
+int builtIn(char* userInput);
 
 int main()
 {
     // Create space in memory for the commands
-    char userInput[100 * sizeof(char)];
+    char userInput[100];
     interface();
     
     do
     {
-        // Gets the user option - Limited it to 100 characters
+        printf("virtual shell > ");
+        // Gets the user input - Limited it to 100 characters
         fgets(userInput, 100, stdin);
         userInput[strcspn(userInput, "\n")] = 0; // removes \n
 
 
+        int status = 0;
         switch (builtIn(userInput))
         {
         case 1:
@@ -39,30 +41,90 @@ int main()
             printf("Exiting\n");
             exit(1);
             break;
-        // We assume that anything not recognized is a request to open an .exe
+        // We assume that anything not recognized is a request to open an application - The user is responsible for correct name and type.
         default:
-            int forkID = fork();
+            char* start = userInput;
+            char* items[] = {"/", "./", "~"};
+            int hasExecuted = 0;
+            pid_t forkID = fork();
 
-            // Child 
-            if (forkID == 0) // Transform child process into new process (app)
+            // Transform child into new process
+            if (forkID == 0)
             {
-                char* args[] = { userInput, NULL };
-                printf("Launching:%s\n", userInput);
-                int f = execv(userInput, args);
-                if (f < 0)
+                // Local search - Built in
+                for (int i = 0; i < 3; i++)
                 {
-                    printf("An error has occurred. Code :%d", f);
-                    exit(f);
+                    if (startsWith(userInput, items[i]))
+                    {
+                        char* args[] = { userInput, NULL };
+                        hasExecuted = 1;
+                        printf("\nLaunching:%s\n", userInput);
+                        int f = execv(userInput, args); // Only returns if an error has occured. Error mode? Maybe use perror
+                        if (f < 0)
+                        {
+                            perror("An error has occurred.");
+                            exit(f);
+                        }
+                    }
+                }
+
+                // If command not in built in. AKA hasnt executed yet.
+                if (hasExecuted == 0) 
+                {
+                    // Find path
+                    char path[4000];
+                    strncpy(path, getenv("PATH"), sizeof(path));
+                    char* pathToTry = strtok(path, ":");
+                    char absPath[1024];
+                    while (pathToTry != NULL)
+                    {
+
+                        snprintf(absPath, sizeof(absPath), "%s/%s", pathToTry, userInput);
+                        if (access(absPath, F_OK) == 0)
+                        {
+                            printf("Found");
+                            char* args[] = { absPath, NULL };
+                            int f = execv(absPath, args);
+                            if (f < 0)
+                            {
+                                perror("An error has occurred,");
+                                exit(f);
+                            }
+                        }
+                        pathToTry = strtok(NULL, ":");
+                    }
                 }
             }
+            // Wait for child process to finish
+            else
+            {
+                wait(&status);
+
+                if (WIFEXITED(status))
+                {
+                    printf("Ended normally with status code %d\n", WEXITSTATUS(status));
+                }
+                else
+                {
+                    printf("Ended abruptally - Killed %d\n", WTERMSIG(status));
+                }
+            }
+           
             break;
         }
 
-    } while (true);
+    } while (1); //Invariant - I want the program to always run until exitted
     
     return 0;
 }
 
+// Stack overflow
+int startsWith(const char* str, const char* prefix) {
+    return strncmp(str, prefix, strlen(prefix)) == 0;
+}
+
+
+// Method to just print interface
 void interface()
 {
     printf( "Print the current directory: pwd\n"
@@ -71,13 +133,15 @@ void interface()
             "To exit virtual bash: exit\n");
 }
 
+// Method to check userinput
 int builtIn(char* userInput)
 {
     if (strcmp(userInput, "pwd") == 0 || strcmp(userInput, "ls") == 0)
     {
         return 1;
     }
-    else if (strstr(userInput, "cd") != NULL)
+    // cd must be followed by space
+    else if (strncmp(userInput, "cd", 2) == 0 && userInput[2] == ' ')
     {
         return 2;
     }
